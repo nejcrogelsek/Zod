dc5f771de3c1a28c947f68640143ff7fa429feb2
nejcrogelsek <nejcrogelsek0@gmail.com>
2023-01-10 10:45:21 +0100

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>ZOD tutorial</h1>
+      <a href='https://zod.dev/' target='_blank'>Documentation</a>
+    </div>
+  )
+}
+
+export default App
