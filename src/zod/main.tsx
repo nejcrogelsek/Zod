e46190a8282002aa0f2a504c6a0f87ebff9028c6
nejcrogelsek <nejcrogelsek0@gmail.com>
2023-01-16 20:35:02 +0100

    Update Copy and Find

--- src/zod/main.tsx
+++ src/zod/main.tsx
@@ -1,7 +1,6 @@
 /*
     Copy and Find:
         Basic types
-        superRefine
         Schema .passthrough(), .strict()
         Array & Tuple
         Union, Discriminated Union
@@ -10,7 +9,7 @@
         Map Type
         Set Type
         Promise Type
-        Advance validation
+        Advance validation (refine, superRefine)
         Error handling
 */
 
@@ -89,27 +88,6 @@ console.log('Log-5: safeParse', UserSchema2.safeParse(user3)) // Success
 console.log('Log-6: safeParse', UserSchema2.shape) // Type of all the keys inside schema
 console.log('Log-7: safeParse', UserSchema2.shape.age) // Type of specified key inside schema
 console.log('Log-8: safeParse', UserSchema2.partial().safeParse(user)) // Success: All of the keys are optional
-
-// superRefine
-
-export const registerUserSchema = z.object({
-    firstName: z.string(),
-    lastName: z.string(),
-    userName: z.string(),
-    email: z.string().email(),
-    phone: z.string().transform(data => Number(data)),
-    password: z.string().min(4),
-    confirmPassword: z.string().min(4),
-    avatar: z.string().optional(),
-    isVerified: z.boolean().optional()
-}).superRefine(({ confirmPassword, password }, ctx) => {
-  if (confirmPassword !== password) {
-    ctx.addIssue({
-      code: "custom",
-      message: "The passwords did not match"
-    });
-  }
-});
 
 // Schema .passthrough(), .strict()
 
@@ -206,7 +184,26 @@ const p = Promise.resolve('resolved')
 
 console.log('Log-19: Promise type', PromiseSchema.safeParse(p)) // Success
 
-// Advance validation
+// Advance validation (refine, superRefine,...)
+
+export const registerUserSchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    userName: z.string(),
+    email: z.string().email(),
+    phone: z.string().transform(data => Number(data)),
+    password: z.string().min(4),
+    confirmPassword: z.string().min(4),
+    avatar: z.string().optional(),
+    isVerified: z.boolean().optional()
+}).superRefine(({ confirmPassword, password }, ctx) => {
+  if (confirmPassword !== password) {
+    ctx.addIssue({
+      code: "custom",
+      message: "The passwords did not match"
+    });
+  }
+});
 
 const brandEmail = z
     .string()
